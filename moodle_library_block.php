7376db3cf97524c875e1ff9bd2899299bd80ad28
lstethers <32719652+lstethers@users.noreply.github.com>
2023-03-24 15:01:35 -0400

    Update moodle_library_block.php
    removed library chat

--- moodle_library_block.php
+++ moodle_library_block.php
@@ -1,6 +1,8 @@
 <?php
 // class block_wes__library_main extends block_base
 // LSS updated 2019.12.17 for new web site links and properly branded chat block
+// LSS updated 2023.03.23 to remove chat block and just include direct contact info
+
 class block_wes_library_main extends block_base
 {
 	public function init() 
@@ -23,19 +25,14 @@ class block_wes_library_main extends block_base
 <li><a href="http://libguides.wesleyan.edu/citing" target="_blank">How to cite your sources</a></li>
 <li><a href="http://libguides.wesleyan.edu/allguides/howto" target="_blank">Library how-to videos and guides</a></li>
 </ul>
-<!-- Place this div in your web page where you want your chat widget to appear. -->
-<div class="needs-js"></div>
-
-<!-- Place this script as near to the end of your BODY as possible. -->
-<script type="text/javascript">
-  (function() {
-    var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.libraryh3lp.com/js/libraryh3lp.js?5043";
-    var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-  })();
-</script>';
+<p>How to reach us:<br />
+<a href="mailto:reference@wesleyan.edu">reference@wesleyan.edu</a><br />
+<a href="https://www.wesleyan.edu/libr/research/prs-form.html">By Appointment</a><br />
+<a href="tel:860-685-3873">(860) 685-3873</a></p>
+';
 		$this->content->footer = '';
 		return $this->content;
 	} // get_content
 
 } // class
+
